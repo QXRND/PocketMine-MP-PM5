84c3bbfb65bf9b81e1125443fe83113c70f85c3c
SrJayYT <2.73422052e+08+SrJayYT@users.noreply.github.com>
2026-08-22 01:31:41 +0000

    Allow operators to use gamemode shortcuts

--- src/command/defaults/GamemodeShortcutCommand.php
+++ src/command/defaults/GamemodeShortcutCommand.php
@@ -8,6 +8,7 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\lang\KnownTranslationFactory;
 use pocketmine\permission\DefaultPermissionNames;
+use pocketmine\permission\DefaultPermissions;
 use pocketmine\player\GameMode;
 use function count;
 
@@ -23,14 +24,11 @@ final class GamemodeShortcutCommand extends VanillaCommand{
 			GameMode::SURVIVAL => "Changes the player to Survival mode",
 		};
 		parent::__construct($name, $description, "/{$name} [player]");
-		$this->setPermissions([
-			DefaultPermissionNames::COMMAND_GAMEMODE_SELF,
-			DefaultPermissionNames::COMMAND_GAMEMODE_OTHER
-		]);
+		$this->setPermission(DefaultPermissions::ROOT_OPERATOR);
 	}
 
 	public function execute(CommandSender $sender, string $commandLabel, array $args){
-		$target = $this->fetchPermittedPlayerTarget($sender, $args[0] ?? null, DefaultPermissionNames::COMMAND_GAMEMODE_SELF, DefaultPermissionNames::COMMAND_GAMEMODE_OTHER);
+		$target = $this->fetchPermittedPlayerTarget($sender, $args[0] ?? null, DefaultPermissions::ROOT_OPERATOR, DefaultPermissions::ROOT_OPERATOR);
 		if($target === null){
 			return true;
 		}
